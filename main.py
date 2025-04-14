86a40d9fd07cd1483586c2a578c2797ff536f856
lucas henrique <lucashasfj2006@gmail.com>
2025-04-14 19:11:49 -0300

    inicial

---
+++ main.py
@@ -0,0 +1,41 @@
+import visualizar, adicionar, remover, buscar, os, questionary
+while True:
+    print("--------------------------------")
+    print("|{:^30}|".format("STOCK CENTER"))
+    print("--------------------------------")
+    print("|{:^30}|".format("AÇÕES DISPONÍVEIS"))
+
+    print("|{:^30}|".format("1 - Visualizar estoque"))
+    print("|{:^30}|".format("2 - Adicionar item"))
+    print("|{:^30}|".format("3 - Remover item"))
+    print("|{:^30}|".format("4 - Buscar item"))
+    print("|{:^30}|".format("5 - Adicionar local"))
+    print("|{:^30}|".format("6 - Sair"))
+    print("--------------------------------")
+
+    resposta = questionary.select('Escolha uma opção:', choices=[
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6'
+    ]).ask()
+
+    if resposta == "1":
+        pass
+    elif resposta == "2":
+        pass
+    elif resposta == "3":
+        pass
+    elif resposta == "4":
+        pass
+    elif resposta == "5":
+        pass
+    elif resposta == "6":
+        print("Sessão encerrada!")
+        exit()
+    else:
+        print("Valor inválido")
+
+
