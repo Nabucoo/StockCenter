a59d3960fc5b0889b11d9b244c7a3e5d6b264e1a
Lucas <lucashasfj2006@gmail.com>
2025-05-16 07:17:59 -0300

    Atualizacao no visualizar

--- visualizar.py
+++ visualizar.py
@@ -1,2 +1,14 @@
-def visualizar_estoque(estoque): 
-    print(estoque)
+
+def visualizar_estoque(estoque, caminho=""): 
+    if estoque:
+        caminho_atual = f'{caminho}/{estoque["nome"]}' if caminho else estoque["nome"]
+        print(f"Local: {caminho_atual}")
+        for item in estoque["itens"]:
+            print(f"\tNome: {item["nome"]}")
+            print(f"\t\tQuantidade: {item["quantidade"]}")
+
+        if estoque["espacos"]:
+            for espaco in estoque["espacos"]:
+                visualizar_estoque(espaco, caminho_atual)
+    else:
+        print("Estoque Vazio!")
