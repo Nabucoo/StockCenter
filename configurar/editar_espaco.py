e1610a2873627d5faee6c6f4fd1692f1874259c2
LuizzGustavoo <luizz.gustavo0.oliveira98@gmail.com>
2025-06-10 22:44:54 -0300

    criação do editar item

--- configurar/editar_espaco.py
+++ configurar/editar_espaco.py
@@ -1,3 +1,10 @@
-def editar_espaco(estoque):
-    print("editando")
-    return
+'''import questionary
+from configurar import buscar_espaco
+
+def editar_espaço(estoque):
+    nome_espaco = input("Digite o nome do espaço que deseja editar:\n").lower()
+    resultado_busca = buscar_espaco.buscar_espaco(estoque, nome_espaco)
+
+    if not resultado_busca:
+        print("Espaço não encontrado!")
+        return'''
